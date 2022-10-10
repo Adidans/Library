aed0b40bda00f5dd0f81d2747a1cfbcd87ee4133
Adidans <adrians.uvis@gmail.com>
2022-10-10 16:41:44 +0300

    Add modal to add books

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 let myLibrary = [];
 
-let bookSection = document.querySelector('.book-section');
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+});
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.newBook.active')
+    modals.forEach(modal =>{
+        closeModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.newBook')
+        closeModal(modal)
+    })
+});
+
+function openModal(modal) {
+    if(modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if(modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+const bookSection = document.querySelector('.book-section');
 
 let book = new Book("bla", "b. la", 321, false)
 let other = new Book("other", "other", 123, true)
@@ -8,6 +45,7 @@ let otherOther = new Book("otherother", "otherother", 456, true)
 addBookToLibrary(book)
 addBookToLibrary(other)
 addBookToLibrary(otherOther)
+displayLibrary();
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -20,6 +58,19 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+function openForm() {
+    document.querySelector(".newBook").style.display = "block";
+}
+function closeForm() {
+    document.querySelector(".newBook").style.display = "none";
+}
+
+window.onclick = function (event) {
+    let modal = document.getElementById('loginPopup');
+    if (event.target == modal) {
+      closeForm();
+    }
+}
 function displayLibrary() {
     for(let i = 0; i < myLibrary.length; i++){
         let card = document.createElement('div')
